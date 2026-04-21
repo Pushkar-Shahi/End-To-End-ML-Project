0b57a19ffc70f73621bd0ff7e7f4a3cc3cf89de4
Pushkar-Shahi <riseinspirethrive2008@gmail.com>
2026-04-21 07:33:17 +0530

    logging and exception

---
+++ src/Pipeline/exception.py
@@ -0,0 +1,8 @@
+import sys
+
+def  error_message_details(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exe_info()
+    error_messages="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+
+        
+    )
